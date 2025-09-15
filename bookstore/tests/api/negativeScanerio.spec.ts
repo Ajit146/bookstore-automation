99a5eecace14e515761f03e5c2c6afb2f8bd482e
Ajit146 <ajitsangwan1993@gmail.com>
2025-09-15 16:20:19 +0530

    added test data file and updated hard coded base url

--- bookstore/tests/api/negativeScanerio.spec.ts
+++ bookstore/tests/api/negativeScanerio.spec.ts
@@ -1,16 +1,16 @@
 import { test, expect } from '@playwright/test';
-import { getHeaders } from './helper';
+import { getHeaders, getBaseURL } from "../config/config";
 
 test.describe('Books API - Negative Scenarios', () => {
   test('Fail to create book without token', async ({ request }) => {
     const payload = { id: 0, name: 'Unauthorized Book' };
-    const response = await request.post('http://127.0.0.1:8000/books', { data: payload });
+    const response = await request.post(`${getBaseURL()}/books`, { data: payload });
     expect([401, 403]).toContain(response.status());
   });
 
   test('Fail to create book with invalid payload', async ({ request }) => {
     const payload = { wrongField: 'oops' };
-    const response = await request.post('http://127.0.0.1:8000/books', {
+    const response = await request.post(`${getBaseURL()}/books`, {
       data: payload,
       headers: getHeaders(),
     });
@@ -18,15 +18,15 @@ test.describe('Books API - Negative Scenarios', () => {
   });
 
   test('Fail to fetch non-existing book ID', async ({ request }) => {
-    const response = await request.get('http://127.0.0.1:8000/books/99999', {
+    const response = await request.get(`${getBaseURL()}/books/99999`, {
       headers: getHeaders(),
     });
     expect([404, 400]).toContain(response.status());
   });
 
-  test('Fail to delete already deleted book', async ({ request }) => {
+  test('Fail to delete non-existing book', async ({ request }) => {
     // try deleting something invalid
-    const response = await request.delete('http://127.0.0.1:8000/books/99999', {
+    const response = await request.delete(`${getBaseURL()}/books/99999`, {
       headers: getHeaders(),
     });
     expect([404, 400]).toContain(response.status());
---
+++ bookstore/tests/data/booksdata.ts
@@ -0,0 +1,14 @@
+export const createBookPayload = {
+  id: 25,
+  name: 'Playwright Automation',
+  author: 'Automation Tester',
+  published_year: 2025,
+  book_summary: 'A book about API automation using Playwright',
+};
+
+export const updateBookPayload = {
+  name: 'Updated Playwright Book',
+  author: 'Automation Tester',
+  published_year: 2026,
+  book_summary: 'Updated summary',
+};
